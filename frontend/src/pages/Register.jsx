0a02c6c28b4666b740750c3029b72688f4cb607a
Ravindra-jingar <ravindrajingar003@gmail.com>
2026-06-08 12:08:27 +0530

    fix Button not define

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -5,6 +5,7 @@ import AuthLayout from "../components/Layout/AuthLayout";
 import AuthInput from "../components/ui/AuthInput";
 import { registerUser} from "../services/authService";
 import Spinner from "../components/ui/Spinner";
+import { Button } from "../components/ui/Button"
 function Register() {
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
